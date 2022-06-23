57373b629a0c5e4d1a5087edc7bec9f54bf43069
Thierno <thierno1492@gmail.com>
2022-06-23 18:17:44 +0200

    infos

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -4,17 +4,17 @@
    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="../assets/item.jpg" alt="First slide">
+      <img class="d-block w-100" src="https://curious-fox-r9fg36-dev-ed--c.documentforce.com/sfc/dist/version/renditionDownload?rendition=ORIGINAL_Jpg&versionId=0687Q000000VqCf&operationContext=DELIVERY&contentId=05T7Q000009auql&page=0&d=/a/7Q000000GoXX/g9lDlnuOzMcSQ3yjaZRgPG0QjEHlsG1A44rltkAYN.Y&oid=00D7Q000004syll&dpt=null&viewId=" alt="First slide">
    
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/WIN_20220202_14_58_28_Pro.jpg" alt="Second slide">
+      <img class="d-block w-100" src="https://curious-fox-r9fg36-dev-ed--c.documentforce.com/sfc/dist/version/renditionDownload?rendition=ORIGINAL_Jpeg&versionId=0687Q000000VqCV&operationContext=DELIVERY&contentId=05T7Q000009auqb&page=0&d=/a/7Q000000GoXb/MGKT1DkAPhHpPCaIIOCO6I8xLkPENYqtzpTDCBnE3B8&oid=00D7Q000004syll&dpt=null&viewId=" alt="Second slide">
       
     </div>
 
     
     <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/item.jpg" alt="Third slide">
+      <img class="d-block w-100" src="https://curious-fox-r9fg36-dev-ed--c.documentforce.com/sfc/dist/version/renditionDownload?rendition=ORIGINAL_Jpg&versionId=0687Q000000Vqij&operationContext=DELIVERY&contentId=05T7Q000009axTa&page=0&d=/a/7Q000000GoXq/7hWwiZ0egQppPFmSQQWAtF_xrGhka16rnEST9tIXjSQ&oid=00D7Q000004syll&dpt=null&viewId=">
     </div>
    
   </div>
